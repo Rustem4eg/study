d4c6429e3c7a3eb481d9d5371fa8aacc6c38c735
Rustem4eg <rus13-13v@mail.ru>
2025-01-06 18:40:27 +0300

    добавлено задание урока 7 - класс очередь

--- Module_4/m4_les7/code_les7.py
+++ Module_4/m4_les7/code_les7.py
@@ -1,32 +1,27 @@
-# Создайте класс TaskQueue, который будет представлять очередь задач. Этот класс должен иметь следующие методы:
-
-# add_task(task): Добавляет задачу task в конец очереди.
-# get_next_task(): Извлекает и возвращает задачу из начала очереди. Если очередь пуста, вернуть None.
-# is_empty(): Проверяет, пуста ли очередь. Возвращает True, если очередь пуста, и False в противном случае.
-# Создайте класс Task, который будет представлять задачу. Этот класс должен иметь атрибут:
-
-# name: Название задачи (строка).
-# Реализуйте логику работы с задачами в соответствии с их добавлением и извлечением из очереди.
 class TaskQueue:
     def __init__(self):
         self.queue = []
 
+
     def add_task(self, task):
         return self.queue.append(task)
-    
+
+
     def get_next_task(self):
         if len(self.queue) > 0:
             first_task = self.queue.pop(0)
             return first_task
         else:
             return None
-        
+
+
     def is_empty(self):
         if not self.queue:
             return True
         else:
             return False
-        
+
+
 class Task:
     def __init__(self, name):
         self.name = name
