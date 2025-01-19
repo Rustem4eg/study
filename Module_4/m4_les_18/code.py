d663de53c2cd4d437550dd56a4bad1d5e358441b
Rustem4eg <rus13-13v@mail.ru>
2025-01-19 11:10:18 +0300

    добавлен код к решению 18

---
+++ Module_4/m4_les_18/code.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Пример использования:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+selection_sort(my_list)
+print("Отсортированный список:", my_list)
