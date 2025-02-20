635874c1e651a899f1206c4e55748b74bc85065d
Rustem4eg <rus13-13v@mail.ru>
2025-02-20 15:47:14 +0300

    добавлены решения  урока 12  модуля 6

--- Module_6/les_12_sql/code.py
+++ Module_6/les_12_sql/code.py
@@ -6,7 +6,7 @@ BASE_DIR = CURRENT_FILE.parent
 SQL_BASE = BASE_DIR / 'library.db'
 
 # Создание базы данных и подключение к ней
-conn = sqlite3.connect(SQL_BASE)
+conn = sqlite3.connect('library.db')
 cursor = conn.cursor()
 
 # Создание таблицы books
@@ -49,29 +49,29 @@ def delete_book(book_id):
     conn.commit()
 
 # Тестовый код
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Добавление новых книг
-    add_book('1984', 'George Orwell', 1949)
-    add_book('Animal Farm', 'George Orwell', 1945)
-    add_book('Fahrenheit 451', 'Ray Bradbury', 1953)
+    add_book("1984", "George Orwell", 1949)
+    add_book("Animal Farm", "George Orwell", 1945)
+    add_book("Fahrenheit 451", "Ray Bradbury", 1953)
 
     # Получение и вывод всех книг
     books = get_all_books()
-    print('Все книги:')
+    print("Все книги:")
     for book in books:
         print(book)
 
     # Обновление информации о книге
-    update_book(1, 'Nineteen Eighty-Four', 'George Orwell', 1949)
+    update_book(1, "Nineteen Eighty-Four", "George Orwell", 1949)
     books = get_all_books()
-    print('\nКниги после обновления:')
+    print("\nКниги после обновления:")
     for book in books:
         print(book)
 
     # Удаление книги
     delete_book(2)
     books = get_all_books()
-    print('\nКниги после удаления:')
+    print("\nКниги после удаления:")
     for book in books:
         print(book)
 
